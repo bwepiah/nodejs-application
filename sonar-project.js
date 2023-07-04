12bde59d5c0687090ae4a01427a33c7b88e401ea
bwepiah <134803150+bwepiah@users.noreply.github.com>
2023-07-03 22:13:07 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://44.204.149.194:9000/',
+  serverUrl: 'http://18.206.161.195:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
